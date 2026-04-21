5497f50b3aa378dd4d6a9232cebcacdce2fc34dd
Mostafa Zahra 0x0 <mostafazahra1000gmail.com>
2026-04-21 23:10:28 +0300

    Add initial HTML structure and main JavaScript file to initialize game fetching

---
+++ js/main.js
@@ -0,0 +1,20 @@
+import { getAmanitaGames } from './api.js';
+
+const init = async () => {
+    console.log("Main engine starting...");
+
+try {
+    const games = await getAmanitaGames();
+    if (games && games.length > 0) {
+        console.log("The data has been successfully delivered to the manager.")
+
+        }else {
+            console.warn("No games were received. Please check your internet connection or API key.")
+        }
+    }
+    catch (error) {
+    console.error("Unexpected Runtime Error", error);
+
+    }
+};
+init();
