ccf434372ac3ce903ba9e630fe3ccf0e0c100dc4
Mostafa Zahra 0x0 <mostafazahra1000gmail.com>
2026-04-24 23:12:42 +0300

    Filter out blacklisted games from the Amanita games API response to improve game display functionality.

--- js/api.js
+++ js/api.js
@@ -3,14 +3,28 @@ import { API_KEY } from './config.js';
 const BASE_URL = 'https://api.rawg.io/api/';
 
 export const getAmanitaGames = async () => {
-
+    const blacklist = [
+        "Phonopolis", 
+        "The Quest for the Rest",
+        "Questionaut",
+        "Rocketman VC",
+        "Osada", 
+        "Samorost 3 Demo",
+        "Happy Game :)",
+        "Botanicula",
+        "Samorost 3", 
+    ];
     try {
         const response = await fetch (`${BASE_URL}games?key=${API_KEY}&developers=amanita-design`);
 
         const data = await response.json();
 
-        return data.results;
-    
+        const allGames = data.results;
+            
+        const filteredGames = allGames.filter(game => 
+            !blacklist.includes(game.name)
+        );
+        return filteredGames;    
     }
     catch (error) {
 
